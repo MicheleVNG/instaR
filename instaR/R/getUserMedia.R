d59bfc90637a3d457db298527d3b3736bd721848
Michele Vangelista <m.vangelista@gmail.com>
2017-03-08 14:34:15 +0100

    Changed getUserMedia

--- instaR/R/getUserMedia.R
+++ instaR/R/getUserMedia.R
@@ -48,7 +48,7 @@ getUserMedia <- function(username, token, n=30, folder=NULL, userid=NULL, verbos
         content <- callAPI(url, token)
         if (length(content$data)==0) stop(c("Error. User name not found. ",
             "Does this application have permission to access public content?"))
-        userid <- as.numeric(content$data[[1]]$id)
+        userid <- as.numeric(content$data$id)
     }
 
     url <- paste0("https://api.instagram.com/v1/users/", userid, "/media/recent?count=",
